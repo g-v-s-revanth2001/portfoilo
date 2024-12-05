a8b8ca9fe578c5a9a52932e0d02b7cb1b43732a3
g-v-s-revanth2001 <83197795+g-v-s-revanth2001@users.noreply.github.com>
2024-12-05 20:55:19 +0530

    Add files via upload

---
+++ portfilo.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+    strings: ['Salesforce Admin.', 'Salesforce Devloper','Java programmer'],
+    typeSpeed: 50,
+  });
+document.querySelector('.name-cer a').addEventListener('click', function(event) {
+    event.preventDefault();
+  });
+  console.log('cc');
